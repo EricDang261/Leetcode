0425c10e30e0a9938bc7e2cfdbbd580393254a7b
Phu Thinh Dang <62625277+EricDang261@users.noreply.github.com>
2022-06-13 11:50:08 -0700

    Time: 6 ms (43.77%), Space: 47.3 MB (55.94%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,31 +1,24 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int minPath = Integer.MAX_VALUE;
-        Integer [][] memo = new Integer[matrix.length][matrix[0].length];
-        int row = 0;
-        for(int j = 0; j < matrix.length; j++){
-            minPath = Math.min(minPath, findMinPath(matrix, row, j, memo));
+         int dp[][] = new int[matrix.length + 1][matrix.length + 1];
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (col == 0) {
+                    dp[row][col] =
+                        Math.min(dp[row + 1][col], dp[row + 1][col + 1]) + matrix[row][col];
+                } else if (col == matrix.length - 1) {
+                    dp[row][col] =
+                        Math.min(dp[row + 1][col], dp[row + 1][col - 1]) + matrix[row][col];
+                } else {
+                    dp[row][col] = Math.min(dp[row + 1][col],
+                        Math.min(dp[row + 1][col + 1], dp[row + 1][col - 1])) + matrix[row][col];
+                }
+            }
         }
-        return minPath;
-    }
-    
-    public int findMinPath(int[][] matrix, int row, int col, Integer[][]memo){
-        // check if we hit out of the boundary of the matrix
-        if(col < 0 || col == matrix.length) return Integer.MAX_VALUE;
-        // check if we hit the last row
-        if(row == matrix.length -1){
-            return matrix[row][col];
+        int minFallingSum = Integer.MAX_VALUE;
+        for (int startCol = 0; startCol < matrix.length; startCol++) {
+            minFallingSum = Math.min(minFallingSum, dp[0][startCol]);
         }
-        // check if we have compute this result before
-        if(memo[row][col] != null){
-            return memo[row][col]; // it has been computed
-        }
-
-        int left = findMinPath(matrix, row+1, col-1, memo);
-        int middle = findMinPath(matrix, row+1 ,col, memo);
-        int right = findMinPath(matrix, row+1, col+1, memo);
-        
-        memo[row][col] = Math.min(left, Math.min(middle, right)) + matrix[row][col];
-        return memo[row][col];
+        return minFallingSum;
     }
 }
