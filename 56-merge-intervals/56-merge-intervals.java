e07532f1036d2d91939ab19f6a5869b68e81d956
Phu Thinh Dang <62625277+EricDang261@users.noreply.github.com>
2022-06-26 23:08:08 -0700

    Time: 16 ms (33.55%), Space: 56.1 MB (5.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare (a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merge = new LinkedList<>();
         
-        LinkedList<int[] > merged = new LinkedList<>();
-        
-        for(int[] interval : intervals){
-            if(merged.isEmpty() || merged.getLast()[1] < interval[0]){
-                merged.add(interval);
+        for(int[] currentMeeting : intervals){
+            if(merge.isEmpty() || merge.getLast()[1] < currentMeeting[0]){
+                merge.add(currentMeeting);
             }else{
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+                merge.getLast()[1] = Math.max(merge.getLast()[1], currentMeeting[1]);
             }
         }
-        return merged.toArray(new int[merged.size()][]);
+        return merge.toArray(new int[merge.size()][]);
     }
 }
