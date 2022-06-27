644cfa46c674d6a54590fc949c4643f5e4ba61d9
Phu Thinh Dang <62625277+EricDang261@users.noreply.github.com>
2022-06-27 12:39:51 -0700

    Time: 2 ms (27.31%), Space: 42.1 MB (32.84%) - LeetHub

---
+++ 266-palindrome-permutation/266-palindrome-permutation.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        for(int i = 0 ; i < s.length(); i++){
+            if(!set.add(s.charAt(i))){
+                set.remove(s.charAt(i));
+            }else{
+                set.add(s.charAt(i));
+            }
+        }
+
+        return set.size() <= 1;
+    }
+}
