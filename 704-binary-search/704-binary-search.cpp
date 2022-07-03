090a023de8b08cae459cadf7825ef57fe9e1ef71
Phu Thinh Dang <62625277+EricDang261@users.noreply.github.com>
2022-07-03 00:34:36 -0700

    Time: 62 ms (36.50%), Space: 27.6 MB (63.79%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        while(l <= r){
-            int mid = (l+r)/2;
-            if(nums[mid] > target){
-                r = mid-1;
-            }else if(nums[mid] < target){
-                l = mid + 1;
-            }else{
-                return mid;
-            }
+        int left = 0;
+        int right = nums.size() -1;
+        while(left <= right){
+            int mid = left + (right-left)/2; // avoid overflow 
+            if(nums[mid] == target) return mid; // return index -> return mid
+            else if(nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
         }
-        return -1; // does not exist
+        return -1; // not found
     }
 };
+
+// time complexity is O(logN);
+// space complexity is O(1);
