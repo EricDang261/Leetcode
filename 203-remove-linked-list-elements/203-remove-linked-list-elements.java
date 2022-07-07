645ac080e2ad85322485403eb6ee54455d30c688
Phu Thinh Dang <62625277+EricDang261@users.noreply.github.com>
2022-07-07 12:10:09 -0700

    Time: 2 ms (19.24%), Space: 49 MB (57.58%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinal = new ListNode();
+        sentinal.next = head; 
+        ListNode curr = head;
+        ListNode prev = sentinal;
+        
+        while(curr != null){
+            if(curr.val ==  val){
+                prev.next =  curr.next;
+            }else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return sentinal.next;
+    }
+}
