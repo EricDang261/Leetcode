e425951efa2dd152d2f389e93caa2a9251d46c2b
Phu Thinh Dang <62625277+EricDang261@users.noreply.github.com>
2022-07-19 19:22:36 -0700

    Time: 1 ms (56.63%), Space: 44.4 MB (82.21%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -11,26 +11,25 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if( head == null ) return null;
-        ListNode intersect = find_intersect(head);
-        if(intersect == null) return null;
-        
-        // find the entrance of the cycle
+        if(head == null) return null;
         ListNode ptr1 = head;
-        ListNode ptr2 = intersect;
+        ListNode ptr2 = findTheIntersection(head);
+        if(ptr2 == null) return null;
+        // find the entry of the cycle list
         while(ptr1 != ptr2){
             ptr1 = ptr1.next;
             ptr2 = ptr2.next;
         }
-        return ptr1; // ptr1 == ptr2
+        return ptr1;
     }
-
-    public ListNode find_intersect(ListNode head){
-        ListNode fast = head;
+    
+    // function to find the intersection of fast and slow ptr
+    public ListNode findTheIntersection(ListNode head){
         ListNode slow = head;
-        while(fast!= null && fast.next != null){
-            fast = fast.next.next;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
             slow = slow.next;
+            fast = fast.next.next;
             if(fast == slow){
                 return slow;
             }
