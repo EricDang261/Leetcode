78e1e6113eb7672c68feaa6a1086bd987762a6e3
Phu Thinh Dang <62625277+EricDang261@users.noreply.github.com>
2022-08-04 18:51:10 -0700

    Create greedySolution.java

---
+++ 1710-maximum-units-on-a-truck/greedySolution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+        int unitCount = 0;
+        for (int[] boxType : boxTypes) {
+            int boxCount = Math.min(truckSize, boxType[0]);
+            unitCount += boxCount * boxType[1];
+            truckSize -= boxCount;
+            if (truckSize == 0)
+                break;
+        }
+        return unitCount;
+    }
+}
